670db59ddd56311b1fa93749a28a588548b2bae7
Herok4Build <thomash.johnson261@gmail.com>
2024-02-11 18:06:49 -0500

    Starting to add SQLAlchemy to handle the database.

--- protoapp/app_run.py
+++ protoapp/app_run.py
@@ -1,13 +1,31 @@
 from flask import Flask, request
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+
+def initiate_db(path = None):
+    # Set up path for database
+    if path == None:
+        path = "./db/"
+        if os.path.isdir(path) == False:
+            os.mkdir(path)
+    db_extend
+    
+
+
 @app.route("/")
 def home():
+    # Render the home.html page for people to access
+    # Currently the first page they land on
     return render_template("home.html")
 
 @app.route("/login", methods = ["POST"])
+# Login function
+# Need to finish register function first before 
+# continuoing with login function
 def login():
     email_var = request.form["useremail"]
     pass_var = request.form["userpass"]
