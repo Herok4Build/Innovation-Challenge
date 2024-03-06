1164d0d47ab696aba8b90870b9b0dffc3519edd8
Herok4Build <thomash.johnson261@gmail.com>
2024-03-06 18:00:14 -0500

    Update the student dashboard.

--- protoapp/app_run.py
+++ protoapp/app_run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, session
 from flask import render_template
+from flask import Response
 from typing import List
 from typing import Optional
 from sqlalchemy import ForeignKey
@@ -15,8 +16,13 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
 import os
-import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 import numpy as np
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+import base64
+from PIL import Image
 
 from sqlalchemy import create_engine
 engine = create_engine("sqlite://", echo=True)
@@ -29,6 +35,76 @@ def total_credits(prev_status = "MS"):
         return(42)
     else:
         return(65)
+
+def total_milestones(prev_status = "MS"):
+    if prev_status == "MS":
+        return(10)
+    else:
+        return(12)
+
+# Reference: https://buraksenol.medium.com/pass-images-to-html-without-saving-them-as-files-using-python-flask-b055f29908a
+# Author: Burak Senol
+# Reference: https://www.geeksforgeeks.org/donut-chart-using-matplotlib-in-python/
+# Author: srivastava
+# Reference: https://matplotlib.org/stable/gallery/user_interfaces/canvasagg.html
+
+# Function based off of Matplotlib documentation to label the values of the donut chart
+def percent_disp(pct, values):
+    actual = int(np.round(pct/100.*np.sum(values)))
+    return f"{pct:.1f}%\n({actual:d})"
+
+# Function to draw donut chart for the number of credits
+# completed_cred is for the number of completed credits
+# total_credits is the total credits necessary for graduation
+def create_credit_chart(completed_cred = 0, total_credits = 42):
+    # Get the number of remaining credits
+    remaining_credits = total_credits - completed_cred
+    # List containing the info on the credits
+    credits_data = [completed_cred, remaining_credits]
+    # colors for the portions of the chart
+    chart_colors = ["#0000FF", "#808080"]
+    # Create the figure
+    credit_fig = Figure()
+    # Establish the subplot
+    axis = credit_fig.add_subplot(1,1,1)
+    # Set the title
+    axis.set_title("My Progress")
+    # Adjust to accomodate the legend
+    credit_fig.subplots_adjust(right=0.6)
+    # Include the data and establish labels
+    wedge_elem, text_elem, autotexts = axis.pie(x = credits_data, colors = chart_colors, autopct = lambda pct: percent_disp(pct,credits_data))
+    # Draw a circle to make a donut
+    axis.pie(x = [100], colors = ["white"], radius =0.80)
+    # Establish the legend for the plot
+    axis.legend(wedge_elem, ["completed", "remaining"], title="Credits",loc = "center left", bbox_to_anchor = (1,0,0.5,1))
+    # Store to memory and save to get around saving as a file
+    outputted_info = io.BytesIO()
+    credit_fig.savefig(outputted_info,format="png")
+    encoded_chart = base64.b64encode(outputted_info.getvalue())
+    decode_chart = encoded_chart.decode("utf-8")
+    # return the necessary encoded information
+    return(decode_chart)
+
+
+# Based on the create_credit_chart() function
+# Uses milestones to calculate the completion progress.
+def create_completion_chart(completed_miles = 0, total_miles = 10):
+    # Calculate the remaining milestones
+    remaining_miles = total_miles - completed_miles
+    # Put milestones data into a list
+    miles_data = [completed_miles, remaining_miles]
+    chart_colors = ["#0000FF", "#808080"]
+    credit_fig = Figure()
+    axis = credit_fig.add_subplot(1,1,1)
+    credit_fig.subplots_adjust(right=0.6)
+    wedge_elem, text_elem = axis.pie(x = credits_data,autopct = lambda pct: percent_disp(pct,miles_data), colors = chart_colors, title="My Completion")
+    axis.pie(x = [100], colors = ["white"], radius =0.80)
+    axis.legend(wedge_elem, ["completed", "remaining"], title="Credits",loc = "center left", bbox_to_anchor = (1,0,0.5,1))
+    outputted_info = io.BytesIO()
+    credit_fig.savefig(outputted_info,format="png")
+    encoded_chart = base64.b64encode(outputted_info.getvalue())
+    decode_chart = encoded_chart.decode("utf-8")
+    return(decode_chart)
 
 class Base(DeclarativeBase):
     pass
@@ -172,6 +248,8 @@ def login():
     pass_var = request.form["userpass"]
     return email_var, pass_var
 
+# Reference: https://stackoverflow.com/questions/50728328/python-how-to-show-matplotlib-in-flask
+# Author: Messa
 @app.route("/student-dash")
 def dashboard():
     if session is not None and "banner_id" in session:
@@ -179,12 +257,12 @@ def dashboard():
         # Get some information form the database
     else:
         student_id = None
-        fig, ax = plt.subplots(figsize=(4,3), subplot_kw=dict(aspect="equal"))
-        completed_credits = 0
-        remaining_credits = total_credits() - completed_credits
-        
+        credits_figure = create_credit_chart()
+        milestones_figure = create_completion_chart()
+        # FigureCanvas(credits_figure).print_png(outputted_info)
+        #credits_chart = Response(outputted_info.getvalue(), mimetype = "image/png")
 
-    return render_template("student-dashboard.html")
+    return render_template("student-dashboard.html", credits_plot =credits_figure, milestones_plot = milestones_figure)
     
 
 @app.route("/existing-user")
